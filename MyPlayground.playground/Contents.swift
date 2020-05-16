077bd22fbf78cc374f339e6c7b0e73f1a80d5795
Ismayil Tahmazov <53227590+IsmayilOfficial@users.noreply.github.com>
2020-05-16 22:43:21 +0300

    first lesson variables

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+var str = "Hello world, this is my first swift code, I am here ura"
+str="hello world"
+print(str)
+var a=1
+var b=12
+a=24
+a=b
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+let x=12
+//x=13 wrong
