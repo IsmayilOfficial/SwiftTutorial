3563d1c95b39ac123a9c8cb03426cce9e195cd4e
Ismayil Tahmazov <53227590+IsmayilOfficial@users.noreply.github.com>
2020-05-24 20:46:57 +0300

    repeat loop

--- MyPlaygroundWhile.playground/Contents.swift
+++ MyPlaygroundWhile.playground/Contents.swift
@@ -5,5 +5,11 @@ var counter=10
 while counter>0
 {
     print(str)
+    print(counter)
     counter=counter-1
 }
+repeat{
+    print(str)
+    print(counter)
+    counter=counter-1
+}while counter>0
