31e7b205bad5911873a72917b93bf5ac1e23c821
Ismayil Tahmazov <53227590+IsmayilOfficial@users.noreply.github.com>
2020-05-25 06:26:14 +0300

    functions part 1

---
+++ functionsMyPlayground.playground/Contents.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+var str = "Hello, playground"
+let a=12
+let c=15
+
+
+func addtwonumbers(){
+    
+    
+    let e=a+c
+    print(e)
+}
+func cutNumbers(){
+    let x=c-a
+    print(x)
+}
+addtwonumbers()
+cutNumbers()
