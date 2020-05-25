7d703c1a9738aac53e7b67554abcf039d3c88629
Ismayil Tahmazov <53227590+IsmayilOfficial@users.noreply.github.com>
2020-05-26 00:48:29 +0300

    Class

---
+++ Classpart1.playground/Contents.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+var str = "Hello, playground"
+class BlogPost{
+    
+   var title = ""
+   var body = ""
+   var author = ""
+    
+    
+    
+}
+let mypost = BlogPost()
+mypost.title = "Test"
+mypost.body = "Xcode is cool"
+mypost.author = "Ismayil"
+print(mypost.body)
