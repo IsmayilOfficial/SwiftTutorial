79fbff8e3db906c17265984d7f6909dc33edf01d
Ismayil Tahmazov <53227590+IsmayilOfficial@users.noreply.github.com>
2020-05-25 06:35:50 +0300

    functions part 2

---
+++ functionspart2.playground/Contents.swift
@@ -0,0 +1,23 @@
+import UIKit
+
+var str = "Hello, playground"
+
+func addTwoNumbers() -> Int {
+    
+    let a = 34
+    let  b = 36
+    return a+b
+}
+let sum = addTwoNumbers()
+print(sum)
+
+
+func addTwoNumbers2(a:Int ,b:Int) -> Int {
+    
+   
+    return a+b
+}
+let sum2 = addTwoNumbers2(a:2,b:2)
+print(sum2)
+
+
